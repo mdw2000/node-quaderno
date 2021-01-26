fc030dd47a96ff778e54c90ddbec4d85869b6687
mdw2000 <meiringdewet1@gmail.com>
2021-01-26 14:50:13 +0200

    refactor: replaced deprecated request with node-fetch

--- src/index.js
+++ src/index.js
@@ -1,160 +1,152 @@
-const request = require('request');
-const assert = require('assert');
+const fetch = require("node-fetch");
+
 const url = require('url');
 const resources = require('./resources');
 const QuadernoError = require('./errors/error');
 
 class Quaderno {
-  constructor(options) {
-    this.validateOptions(options);
-    this.requestConfig = this.getRequestConfig(options.privateApiKey);
-    this.baseUrlObject = this.getBaseUrlObject(options.accountName,
-      options.privateApiKey);
+    constructor(options) {
+        this.validateOptions(options);
+        this.baseUrlObject = this.getBaseUrlObject(options);
 
-    this.contacts = new resources.Contacts(this);
-    this.receipts = new resources.Receipts(this);
-    this.invoices = new resources.Invoices(this);
-    this.credits = new resources.Credits(this);
-    this.expenses = new resources.Expenses(this);
-    this.estimates = new resources.Estimates(this);
-    this.recurring = new resources.Recurring(this);
-    this.items = new resources.Items(this);
-    this.payments = new resources.Payments(this);
-    this.taxes = new resources.Taxes(this);
-    this.evidence = new resources.Evidence(this);
-    this.webhooks = new resources.Webhooks(this);
-  }
-
-  /**
-   * Validate the provided options object against certain rules.
-   * The `accountName` and `privateApiKey` properties have 
-   * mandatory String values.
-   * @param {Object} [options] Quaderno init options.
-   */
-  validateOptions(options) {
-    const { accountName, privateApiKey } = options;
-    if (!accountName || typeof accountName !== 'string') {
-      throw new Error('No accountName is provided');
+        this.contacts = new resources.Contacts(this);
+        this.receipts = new resources.Receipts(this);
+        this.invoices = new resources.Invoices(this);
+        this.credits = new resources.Credits(this);
+        this.expenses = new resources.Expenses(this);
+        this.estimates = new resources.Estimates(this);
+        this.recurring = new resources.Recurring(this);
+        this.items = new resources.Items(this);
+        this.payments = new resources.Payments(this);
+        this.taxes = new resources.Taxes(this);
+        this.evidence = new resources.Evidence(this);
+        this.webhooks = new resources.Webhooks(this);
     }
-    if (!privateApiKey || typeof privateApiKey !== 'string') {
-      throw new Error('No privateApiKey is provided');
-    }
-  }
 
-  /**
-   * Returns a configuration object which can be used when
-   * performing a HTTP request to the Quaderno API.
-   * @param {String} [privateApiKey] Private Quaderno API key.
-   * @return {Object}
-   */
-  getRequestConfig(privateApiKey) {
-    return {
-      json: true,
-      auth: {
-        username: privateApiKey,
-        password: 'x',
-      },
-    };
-  }
+    /**
+     * Validate the provided options object against certain rules.
+     * The `accountName` and `privateApiKey` properties have
+     * mandatory String values.
+     * @param {Object} [options] Quaderno init options.
+     */
+    validateOptions(options) {
+        const {accountName, privateApiKey, token} = options;
 
-  /**
-   * Returns an object containing URL information (a {@link urlObject})
-   * which can be provided to {@link url.format()} to retrieve a
-   * formatted URL string derived from the {@link urlObject}.
-   * @param {String} [accountName] Quaderno account name.
-   * @param {String} [privateApiKey] Private Quaderno API key.
-   * @return {Object}
-   */
-  getBaseUrlObject(accountName, privateApiKey) {
-    const sandbox = /sk_test/.test(privateApiKey);
-    return {
-      protocol: sandbox ? 'http' : 'https',
-      hostname: `${accountName}.${sandbox ? 'sandbox-' : ''}quadernoapp.com/api`,
-    };
-  }
-
-  /**
-   * @param {Object} [urlObject] A URL object (as returned 
-   * by {@link url.parse()} or constructed otherwise).
-   * @return {String} A formatted URL string derived from
-   * {@link urlObject}.
-   */
-  url(urlObject) {
-    return url.format({
-      ...this.baseUrlObject,
-      ...urlObject,
-    });
-  }
-
-  /**
-   * @param {String} [method] The HTTP request method
-   * @param {Object} [urlObject] A URL object (as returned 
-   * by url.parse() or constructed otherwise).
-   * @param {Object} [body]
-   * @param {HTTPStatusCode} [successCode] The HTTP status code that
-   * signifies a successful request.
-   * @return {Promise} The response from the API.
-   */
-  dispatch(method, urlObject, body, successCode=200) {
-    return new Promise((resolve, reject) => {
-      const options = {
-        url: this.url(urlObject),
-        method,
-        body,
-        ...this.requestConfig,
-      };
-
-      request(options, (error, response, body) => {
-        if (error) {
-          return reject(Error(error));
-        } else if (response.statusCode !== successCode) {
-          return reject(new QuadernoError(body));
+        if (token) {
+            if (typeof token !== 'string') {
+                throw new Error('Token must be a string value');
+            }
         } else {
-          return resolve(body);
+            if (!accountName || typeof accountName !== 'string') {
+                throw new Error('No accountName is provided');
+            }
+            if (!privateApiKey || typeof privateApiKey !== 'string') {
+                throw new Error('No privateApiKey is provided');
+            }
         }
-      });
-    });
-  }
+    }
 
-  /**
-   * @param {String} [pathname]
-   * @param {Object} [body]
-   * @return {Promise} The response from the API.
-   */
-  post(pathname, body) {
-    const urlObject = { pathname };
-    return this.dispatch('POST', urlObject, body, 201);
-  }
 
-  /**
-   * @param {String} [pathname]
-   * @param {Object} [body]
-   * @return {Promise} The response from the API.
-   */
-  get(pathname, query) {
-    const urlObject = { pathname, query };
-    return this.dispatch('GET', urlObject, undefined, 200);
-  }
+    /**
+     * Returns an object containing URL information (a {@link urlObject})
+     * which can be provided to {@link url.format()} to retrieve a
+     * formatted URL string derived from the {@link urlObject}.
+     * @param {Object} [options] Quaderno constructor options.
+     * @return {Object}
+     */
+    getBaseUrlObject(options) {
+        const sandbox = options.sandbox;
+        if (options.token) {
+            return {
+                auth: `Bearer ${token}`,
+                protocol: sandbox ? 'http' : 'https',
+                hostname: `${sandbox ? 'sandbox-' : ''}quadernoapp.com/api`,
+            };
+        } else {
+            const sandbox = /sk_test/.test(options.privateApiKey);
+            return {
+                auth: 'Basic ' + Buffer.from(`${options.privateApiKey}:x`).toString('base64'),
+                protocol: sandbox ? 'http' : 'https',
+                hostname: `${options.accountName}.${sandbox ? 'sandbox-' : ''}quadernoapp.com/api`,
+            };
+        }
+    }
 
-  /**
-   * @param {String} [pathname]
-   * @param {Object} [body]
-   * @return {Promise} The response from the API.
-   */
-  put(pathname, body) {
-    const urlObject = { pathname };
-    return this.dispatch('PUT', urlObject, body, 201);
-  }
+    /**
+     * @param {Object} [urlObject] A URL object (as returned
+     * by {@link url.parse()} or constructed otherwise).
+     * @return {String} A formatted URL string derived from
+     * {@link urlObject}.
+     */
+    url(urlObject) {
+        return url.format({
+            ...this.baseUrlObject,
+            ...urlObject,
+        });
+    }
 
-  /**
-   * @param {String} [pathname]
-   * @param {Object} [body]
-   * @return {Promise} The response from the API.
-   */
-  delete(pathname) {
-    const urlObject = { pathname };
-    return this.dispatch('DELETE', urlObject, undefined, 204);
-  }
+    /**
+     * @param {String} [method] The HTTP request method
+     * @param {Object} [urlObject] A URL object (as returned
+     * by url.parse() or constructed otherwise).
+     * @param {Object} [body]
+     * @param {HTTPStatusCode} [successCode] The HTTP status code that
+     * signifies a successful request.
+     * @return {Promise} The response from the API.
+     */
+    async dispatch(method, urlObject, body, successCode = 200) {
+        const rsp = await fetch(this.url(urlObject), {
+            method: method.toLowerCase(),
+            headers: {
+                "Authorization": this.baseUrlObject.auth,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        });
+        const response = await rsp.json();
+        if (rsp.status !== successCode) {
+            throw (new QuadernoError(response));
+        }
+        return response;
+    }
+
+    /**
+     * @param {String} [pathname]
+     * @param {Object} [body]
+     * @return {Promise} The response from the API.
+     */
+    post(pathname, body) {
+        const urlObject = {pathname};
+        return this.dispatch('POST', urlObject, body, 201);
+    }
+
+    /**
+     * @param {String} [pathname]
+     * @return {Promise} The response from the API.
+     */
+    get(pathname, query) {
+        const urlObject = {pathname, query};
+        return this.dispatch('GET', urlObject, undefined, 200);
+    }
+
+    /**
+     * @param {String} [pathname]
+     * @param {Object} [body]
+     * @return {Promise} The response from the API.
+     */
+    put(pathname, body) {
+        const urlObject = {pathname};
+        return this.dispatch('PUT', urlObject, body, 201);
+    }
+
+    /**
+     * @param {String} [pathname]
+     * @return {Promise} The response from the API.
+     */
+    delete(pathname) {
+        const urlObject = {pathname};
+        return this.dispatch('DELETE', urlObject, undefined, 204);
+    }
 }
 
 module.exports = Quaderno;
--- test/index.js
+++ test/index.js
@@ -4,22 +4,60 @@ const rewire = require('rewire');
 const Quaderno = rewire('../');
 
 describe('Quaderno', () => {
-  describe('instantiation', () => {
-    it('should throw an error if accountName is not provided', () => {
-      assert.throws(() => {
-        new Quaderno({
-          privateApiKey: '12345678',
+    describe('instantiation', () => {
+        it('should throw an error if accountName is not provided', () => {
+            assert.throws(() => {
+                new Quaderno({
+                    privateApiKey: '12345678',
+                });
+            }, /^Error: No accountName is provided$/);
         });
-      }, /^Error: No accountName is provided$/);
+
+        it('should throw an error if privateApiKey is not provided', () => {
+            assert.throws(() => {
+                new Quaderno({
+                    accountName: 'nonce',
+                });
+            }, /^Error: No privateApiKey is provided$/);
+        });
     });
 
-    it('should throw an error if privateApiKey is not provided', () => {
-      assert.throws(() => {
-        new Quaderno({
-          accountName: 'nonce',
+    describe('invoice', () => {
+        it('should create an invoice', async () => {
+            const client = new Quaderno({
+                accountName: 'ninive-4990',
+                privateApiKey: 'sk_test_WbnSHMkci7Czxj9f1Jc8',
+            });
+
+            const rsp = await client.invoices.create({
+                    "contact": {
+                        "first_name": "Tony",
+                        "last_name": "Stark",
+                        "email": "tony@tete.te",
+                        "kind": "person", "contact_name": "Stark"
+                    },
+                    "po_number": "",
+                    "currency": "USD",
+                    "country": "NL",
+                    "tag_list": "playboy, businessman",
+                    "items_attributes": [
+                        {
+                            "description": "Whiskey",
+                            "quantity": "1.0",
+                            "unit_price": "20.0",
+                            "discount_rate": "0.0",
+                            "reference": "item_code_X"
+                        }
+                    ],
+                    "custom_metadata": {
+                        "a_custom_key": "a custom value"
+                    }
+                }
+            );
+
+            console.log(rsp);
+
         });
-      }, /^Error: No privateApiKey is provided$/);
     });
-  });
 
 });
